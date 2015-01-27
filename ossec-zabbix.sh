3854ec2745cf5ae2c83b5b49e6c9b8ed381564b6
Rom ioSquare <romainburger@gmail.com>
2015-01-27 16:08:24 +0100

    Added cleaned version

--- ossec-zabbix.sh
+++ ossec-zabbix.sh
@@ -1,24 +1,21 @@
 #!/bin/sh
 
-##############################################################
-# ioSquare: This file is managed by Puppet.                  #
-# WARNING: Any local modification to this file will be lost. #
-##############################################################
-
-# This ossec active response will send the level a crafted message
+# This ossec active response will send the alert level, the message and the last log line
 # To ZBX_SERVER using ZBX_HOSTNAME as the hostname in Zabbix
 
 ACTION=$1
 ALERTID=$4
 RULEID=$5
 
-#Our constant
 
+# Your Zabbix server
 ZBX_SERVER="zabbix.classmatic.net"
-ZBX_HOSTNAME=$(cat /etc/zabbix/zabbix_agentd.conf |grep -E "^Hostname=.+" |cut -d= -f2)
+
+# your hostname
+ZBX_HOSTNAME=$(cat /etc/zabbix/zabbix_agentd.conf |grep -E "^Hostname=.+" |cut -d= -f2) 
 
 
-# Logging the call
+# Logging the call to active-responses
 echo "`date` $0 $1 $2 $3 $4 $5 $6 $7 $8" >> /var/ossec/logs/active-responses.log
 
 
@@ -31,9 +28,11 @@ ALERTLAST=`echo "$ALERTID" | cut -d  "." -f 2`
 # Getting alert level
 alert_level=$(grep -A 4 "$ALERTID" /var/ossec/logs/alerts/alerts.log | grep -v ".$ALERTLAST: " -A 4 | grep -Eo "(level [0-9]+)"| grep -Eo "[0-9]+")
 
+# Now the alert message
 alert_message=$(grep -A 4 "$ALERTID" /var/ossec/logs/alerts/alerts.log | grep -v ".$ALERTLAST: " -A 4 | grep -Eo "\(level [0-9]+\).+" )
 
-# Testing : send the log line too
+# More tricky : get the last log line that caused the alert. 
+# If there is no log line, then we set log_line to "no log"
 line_number=$(( $( cat /var/ossec/logs/alerts/alerts.log | grep -A 15 "$ALERTID" | grep -nE "^$" | head -n1 | cut -d: -f1)-2 ))
 
 alert_log=$( cat /var/ossec/logs/alerts/alerts.log | grep -A $line_number "$ALERTID" | tail -n1 )
@@ -42,13 +41,9 @@ if [ -z "$alert_log" ]; then
 	alert_log="no log"
 fi
 
-
-# If there is more than one alert for a alerttime, then we loop to send all the alerts.
-
-set -e 
-
-	echo "$ZBX_HOSTNAME ossec.last_alert_message $alert_message" | zabbix_sender -z $ZBX_SERVER -i -
-	echo "$ZBX_HOSTNAME ossec.last_alert_level $alert_level" | zabbix_sender -z $ZBX_SERVER -i -
-	echo "$ZBX_HOSTNAME ossec.last_alert_log $alert_log" | zabbix_sender -z $ZBX_SERVER -i -
+# Now use zabbix_sender to send ossec alert information to Zabbix server
+echo "$ZBX_HOSTNAME ossec.last_alert_message $alert_message" | zabbix_sender -z $ZBX_SERVER -i -
+echo "$ZBX_HOSTNAME ossec.last_alert_level $alert_level" | zabbix_sender -z $ZBX_SERVER -i -
+echo "$ZBX_HOSTNAME ossec.last_alert_log $alert_log" | zabbix_sender -z $ZBX_SERVER -i -
 
 
